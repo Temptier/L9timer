5694cdbd5bf1a0d4410cfff1408b052f803a0462
Temptier <mmmcoc3@gmail.com>
2025-11-16 22:39:52 +0800

    Update Firebase.js

--- js/Firebase.js
+++ js/Firebase.js
@@ -4,14 +4,14 @@
 */
 (function(window){
   const firebaseConfig = {
-    apiKey: "AIzaSyAD5I3c-SZ2LRuYG_6kaMgEkjvOtP1d3pU",
-    authDomain: "synchronizedtimer-57ef0.firebaseapp.com",
-    databaseURL: "https://synchronizedtimer-57ef0-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "synchronizedtimer-57ef0",
-    storageBucket: "synchronizedtimer-57ef0.appspot.com",
-    messagingSenderId: "812068089886",
-    appId: "1:812068089886:web:61e95179ca3ec2251e7e0f"
-  };
+      apiKey: "AIzaSyCcZa-fnSwdD36rB_DAR-SSfFlzH2fqcPc",
+  authDomain: "lordninetimer.firebaseapp.com",
+  databaseURL: "https://lordninetimer-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "lordninetimer",
+  storageBucket: "lordninetimer.firebasestorage.app",
+  messagingSenderId: "462837939255",
+  appId: "1:462837939255:web:dee141d630d5d9b94a53b2"
+};
 
   if (!firebase.apps.length) {
     firebase.initializeApp(firebaseConfig);
